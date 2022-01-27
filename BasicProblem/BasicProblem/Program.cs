4e23fdc69e6af96abe93e5c018d6b960de3693c1
tushar <tusharshitre123@gmail.com>
2022-01-27 23:36:42 +0530

    Swaptwonumber

--- BasicProblem/BasicProblem/Program.cs
+++ BasicProblem/BasicProblem/Program.cs
@@ -12,7 +12,7 @@ namespace BasicProblem
         {
             Console.WriteLine("welcome to the basic problem ");
             Flipcoin head = new Flipcoin();
-            head.division();
+            head.Swaping(10,20);
             Console.ReadLine();
 
 
